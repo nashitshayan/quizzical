b7c253f3d88fcb294d397534587f00de63bd495b
Nashit Shayan <shayan.nashit@gmail.com>
2022-06-24 10:20:17 +0530

    Set up user auth context and provider

---
+++ src/context/UserAuthContext.js
@@ -0,0 +1,69 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import {
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+	signOut,
+	onAuthStateChanged,
+	GoogleAuthProvider,
+	signInWithPopup,
+	sendPasswordResetEmail,
+} from 'firebase/auth';
+import { auth } from '../firebase';
+
+// create context
+const userAuthContext = createContext();
+
+// set up the auth context provider
+
+export function UserAuthContextProvider({ children }) {
+	const [user, setUser] = useState('');
+	const [isPending, setIsPending] = useState(true);
+
+	function signUp(email, password) {
+		return createUserWithEmailAndPassword(auth, email, password);
+	}
+	function signIn(email, password) {
+		return signInWithEmailAndPassword(auth, email, password);
+	}
+	function logOut() {
+		return signOut(auth);
+	}
+	function googleSignIn() {
+		const googleAuthProvider = new GoogleAuthProvider();
+		return signInWithPopup(auth, googleAuthProvider);
+	}
+	function sendPasswordReset(email) {
+		return sendPasswordResetEmail(auth, email);
+	}
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+			setIsPending(false);
+		});
+		return () => unsubscribe();
+	}, []);
+	if (isPending)
+		return (
+			<div className='loading-wrapper'>
+				<span>Loading...</span>
+			</div>
+		);
+	return (
+		<userAuthContext.Provider
+			value={{
+				user,
+				signUp,
+				signIn,
+				logOut,
+				googleSignIn,
+				sendPasswordReset,
+			}}>
+			{children}
+		</userAuthContext.Provider>
+	);
+}
+
+export function useUserAuth() {
+	return useContext(userAuthContext);
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import './styles/App.css';
+import { UserAuthContextProvider } from './context/UserAuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<UserAuthContextProvider>
+			<App />
+		</UserAuthContextProvider>
 	</React.StrictMode>,
 );
 
