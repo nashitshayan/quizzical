039db5dcf075a087ce12c6bc41d583991970c7ef
Nashit Shayan <shayan.nashit@gmail.com>
2022-06-25 18:41:55 +0530

    Set up protected route

--- src/components/Quiz/Home.js
+++ src/components/Quiz/Home.js
@@ -1,7 +1,22 @@
 import React from 'react';
-
+import { useUserAuth } from '../../context/UserAuthContext';
 function Home() {
-	return <div>Home</div>;
+	const { logOut } = useUserAuth();
+	const handleLogOut = async () => {
+		try {
+			await logOut();
+		} catch (err) {
+			alert(err);
+		}
+	};
+	return (
+		<div>
+			Home
+			<button onClick={handleLogOut} className='btn-cv-toggle'>
+				Log Out
+			</button>
+		</div>
+	);
 }
 
 export default Home;
