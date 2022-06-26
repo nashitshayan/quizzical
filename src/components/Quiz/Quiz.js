42c3bcfc7db723923d0511375a3117648ac59f38
Nashit Shayan <shayan.nashit@gmail.com>
2022-06-26 13:32:46 +0530

    Style quiz page

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,10 +2,8 @@ import { useLocation } from 'react-router-dom';
 function Quiz() {
 	const location = useLocation();
 	const quizData = location.state;
-	console.log(quizData);
 	const shuffleAnswers = (wrongAnswersArray, correctAnswer) => {
 		const answersArray = [...wrongAnswersArray, correctAnswer];
-		console.log('before', answersArray);
 		for (let i = answersArray.length - 1; i > 0; i--) {
 			const randomIndex = Math.floor(Math.random() * (i + 1));
 			[answersArray[i], answersArray[randomIndex]] = [
@@ -13,19 +11,23 @@ function Quiz() {
 				answersArray[i],
 			];
 		}
-		console.log('after', answersArray);
 		return answersArray;
 	};
 	const quiz = quizData.map((quizItem) => (
-		<div>
-			<h2>Question : {quizItem.question}</h2>
-			<div>
-				{shuffleAnswers(quizItem.incorrect_answers, quizItem.correct_answer)}
+		<div className='quiz-item'>
+			<h3>{quizItem.question}</h3>
+			<div className='quiz-item__choices'>
+				{shuffleAnswers(
+					quizItem.incorrect_answers,
+					quizItem.correct_answer,
+				).map((choice) => (
+					<li className='quiz-item__choices-choice'>{choice}</li>
+				))}
 			</div>
-			<div>correct ans: {quizItem.correct_answer}</div>
+			<hr className='break-line' />
 		</div>
 	));
-	return <div>{quiz}</div>;
+	return <div className='quiz-wrapper'>{quiz}</div>;
 }
 
 export default Quiz;
