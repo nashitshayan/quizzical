d5131e021287864805425aafaae15de4b0db29ee
Nashit Shayan <shayan.nashit@gmail.com>
2022-06-26 12:50:36 +0530

    Shuffle the answers

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,11 +3,25 @@ function Quiz() {
 	const location = useLocation();
 	const quizData = location.state;
 	console.log(quizData);
-
+	const shuffleAnswers = (wrongAnswersArray, correctAnswer) => {
+		const answersArray = [...wrongAnswersArray, correctAnswer];
+		console.log('before', answersArray);
+		for (let i = answersArray.length - 1; i > 0; i--) {
+			const randomIndex = Math.floor(Math.random() * (i + 1));
+			[answersArray[i], answersArray[randomIndex]] = [
+				answersArray[randomIndex],
+				answersArray[i],
+			];
+		}
+		console.log('after', answersArray);
+		return answersArray;
+	};
 	const quiz = quizData.map((quizItem) => (
 		<div>
 			<h2>Question : {quizItem.question}</h2>
-
+			<div>
+				{shuffleAnswers(quizItem.incorrect_answers, quizItem.correct_answer)}
+			</div>
 			<div>correct ans: {quizItem.correct_answer}</div>
 		</div>
 	));
