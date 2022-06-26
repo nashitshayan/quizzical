7ceeb13d9e773ced87005306617ef0c6817161c3
Nashit Shayan <shayan.nashit@gmail.com>
2022-06-26 13:09:15 +0530

    Get only multiple choice questions

--- src/components/Quiz/Home.js
+++ src/components/Quiz/Home.js
@@ -30,7 +30,9 @@ function Home() {
 	const [quizData, setQuizData] = useState([]);
 
 	const handleGetQuiz = async () => {
-		const res = await fetch('https://opentdb.com/api.php?amount=5');
+		const res = await fetch(
+			'https://opentdb.com/api.php?amount=5&type=multiple',
+		);
 		const data = await res.json();
 		setQuizData(data.results);
 	};
