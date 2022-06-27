f65efc9dc35c40e6caf8ae884b2b2500f5e64073
Nashit Shayan <shayan.nashit@gmail.com>
2022-06-27 11:06:59 +0530

    Add custom styles for quiz answers

--- src/styles/inline-styles.js
+++ src/styles/inline-styles.js
@@ -8,3 +8,18 @@ export const inputOutlineOverride = {
 		color: '#293264',
 	},
 };
+
+export const answerSelected = {
+	backgroundColor: '#D6DbF5',
+	color: '#293264',
+};
+
+export const answerCorrect = {
+	backgroundColor: '#94D7A2',
+	color: '#293264',
+};
+
+export const answerWrong = {
+	backgroundColor: '#F8BCBC',
+	color: '#293264',
+};
